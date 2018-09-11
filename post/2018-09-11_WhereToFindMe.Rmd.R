aca6a1196ee5f9721a94a8ef73673de643e9ed96
Katie <resourcefulsquirrel@gmail.com>
2018-09-11 14:07:47 -0500

    updateWebsite

---
+++ post/2018-09-11_WhereToFindMe.Rmd.R
@@ -0,0 +1,8 @@
+---
+title: "Where you can find me this fall"
+author: "Katie"
+date: "2018-09-11"
+output: "html_document"
+---
+  
+This fall, I am working hard at trying to finish my dissertation. This means I'll be spending plenty of time in the lab trying to finish generating my data'
